32a4310880eede01ee6b51dbd4192fc3615b6980
Robert Finkley <rfinkley82@gmail.com>
2021-03-26 23:31:41 -0400

    Added Following profile

---
+++ client/user/FollowingProfileButton.js
@@ -0,0 +1,25 @@
+export default function FollowProfileButton(props) {
+  const followClick = () => {
+    props.onButtonClick(follow);
+  };
+  const unfollowClick = () => {
+    props.onButtonClick(unfollow);
+  };
+  return (
+    <div>
+      {props.following ? (
+        <Button variant="contained" color="secondary" onClick={unfollowClick}>
+          Unfollow
+        </Button>
+      ) : (
+        <Button variant="contained" color="primary" onClick={followClick}>
+          Follow
+        </Button>
+      )}
+    </div>
+  );
+}
+FollowProfileButton.propTypes = {
+  following: PropTypes.bool.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+};
